d8c912c7e195cd4e2e66cfea76ab860e8901b1f7
Peter Williams <pw977618@ohio.edu>
2019-05-16 14:28:30 -0400

    program for homework

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Peter Williams
+ *        Date: 5/16/2019
+ * Description: Outputs "Hello Git!!"
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
